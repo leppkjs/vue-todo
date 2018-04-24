2673c022ee7158db2cdb5734ecf2ae7ced3949bf
RIN JAE <leppk@naver.com>
2018-04-25 00:08:48 +0900

    first commit

---
+++ src/components/TodoInput.vue
@@ -0,0 +1,28 @@
+<template>
+  <div>
+    <input type="text" v-model="newTodoItem">
+    <button v-on:click="addTodo">add</button>
+  </div>
+</template>
+
+<script>
+export default {
+  data: () => {
+    return {
+      newTodoItem : ""
+    }
+  },
+  methods: {
+    addTodo: function() {
+      console.log(this.newTodoItem);
+      //저장하는 로직
+      localStorage.setItem(this.newTodoItem, this.newTodoItem);
+      this.newTodoItem = "";
+    }
+  }
+}
+</script>
+
+<style>
+
+</style>
