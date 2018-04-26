09dd0e41cb66227fa8189f9829f625fb8a8d8db9
김재섭B <jaeseopkim@smilegate.com>
2018-04-26 18:52:08 +0900

    TodoInput complate

--- src/components/TodoInput.vue
+++ src/components/TodoInput.vue
@@ -1,5 +1,5 @@
 <template>
-  <div>
+  <div class="inputBox shadow">
     <input type="text" v-model="newTodoItem">
     <button v-on:click="addTodo">add</button>
   </div>
@@ -14,15 +14,39 @@ export default {
   },
   methods: {
     addTodo: function() {
-      console.log(this.newTodoItem);
-      //저장하는 로직
       localStorage.setItem(this.newTodoItem, this.newTodoItem);
-      this.newTodoItem = "";
+      this.clearInput();
+    },
+    clearInput: function() {
+      this.newTodoItem = '';
     }
   }
 }
 </script>
 
-<style>
-
+<style scoped>
+input:focus {
+  outline: none;
+}
+.inputBox {
+  background: white;
+  height: 50px;
+  line-height: 50px;
+  border-radius: 5px;
+}
+.inputBox input {
+  border-style: none;
+  font-size: 0.9rem;
+}
+.addContainer {
+  float: right;
+  background: linear-gradient(to right, #6478FB, #8763FB);
+  display: block;
+  width: 3rem;
+  border-radius: 0 5px 5px 0;
+}
+.addBtn {
+  color: white;
+  vertical-align: middle;
+}
 </style>
